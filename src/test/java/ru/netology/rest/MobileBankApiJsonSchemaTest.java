e61af6cf20aeed2e835b9e701458dc9644fddef9
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-03-06 02:11:57 +0300

    test updated

--- src/test/java/ru/netology/rest/MobileBankApiJsonSchemaTest.java
+++ src/test/java/ru/netology/rest/MobileBankApiJsonSchemaTest.java
@@ -1,14 +1,12 @@
 package ru.netology.rest;
 
-import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV5 {
+public class MobileBankApiJsonSchemaTest {
+
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
@@ -21,8 +19,6 @@ class MobileBankApiTestV5 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("every{ it.balance >= 0 }", is(true))
-        ;
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
